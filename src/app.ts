d5a1627fb9ab73351c5e7511d83d9ba8a0e549e8
Julien Guitard <julienguitard@luckycart.com>
2024-05-17 11:15:26 +0200

    Start of the migration to ts

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+
+import express, {Application, RequestHandler} from 'express';
+import session, {SessionOptions} from 'express-session';
+import cors from 'cors';
+import helmet from 'helmet';
+import path from 'path';
+import bodyParser from 'body-parser';
+import {router} from './routes/index.js';
+import {IncomingMessage } from 'http';
+
+const app: Application = express();
+const port:number = 3001;
+const sessionOptions: SessionOptions = {
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'keyboard cat'
+};
+const sessionMiddleware:RequestHandler= session(sessionOptions);
+
+//app.use(fooControler);
+
+app.use(express.static('public'));
+app.use(cors());
+app.use(helmet());
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(sessionMiddleware);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+export { app, port };
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { app, port } from './app.js';
 
 app.listen(port);
-console.log(`Express started on port ${port}`);
+console.log(`Express started on port ${port} with tsx!`);
 
